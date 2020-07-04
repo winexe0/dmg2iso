e9b61fb539a6b37effa9dcb78f84fb215b7bf0a6
aryan <47309918+winexe0@users.noreply.github.com>
2020-07-04 11:00:19 -0700

    Update install-windows.bat

--- install-windows.bat
+++ install-windows.bat
@@ -4,4 +4,12 @@ echo Don't change the Installation directory. It will cause problems. When It's 
 echo Check the box on "mingw32-gcc-g++" and click on installation on the upper left corner.
 echo Click on apply changes and click on apply. When it is done, click on close.
 cd C:\MinGW\bin
+Invoke-WebRequest https://raw.githubusercontent.com/winexe0/dmg2iso/master/dmg2iso.cc
+Invoke-WebRequest https://raw.githubusercontent.com/winexe0/dmg2iso/master/dmg2iso.h
+Invoke-WebRequest https://raw.githubusercontent.com/winexe0/dmg2iso/master/base64.h
+Invoke-WebRequest https://raw.githubusercontent.com/winexe0/dmg2iso/master/base64.cc
 gcc -o dmg2iso.exe dmg2iso.o base64.o -L. -lz -s
+gcc -03 -c dmg2iso.cc
+gcc -03 -c base64.cc
+move dmg2iso.exe C:\Windows\System32
+echo You can close this window now
