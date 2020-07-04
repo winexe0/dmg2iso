c00cbc6a0af53e81967f2f26afa86fe383534aca
aryan <47309918+winexe0@users.noreply.github.com>
2020-07-04 10:50:33 -0700

    Update install-windows.bat

--- install-windows.bat
+++ install-windows.bat
@@ -1,2 +1,6 @@
 Invoke-WebRequest https://osdn.net/frs/redir.php?m=xtom_us&f=mingw%2F68260%2Fmingw-get-setup.exe
 mingw-get-setup
+echo Don't change the Installation directory. It will cause problems. When It's done click on continue.
+echo Check the box on "mingw32-gcc-g++" and click on installation on the upper left corner.
+echo Click on apply changes and click on apply. When it is done, click on close.
+cd C:\MinGW\bin
