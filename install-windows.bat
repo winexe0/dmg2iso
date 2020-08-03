22bf7a46682db24c43bdc108d5a422138b491330
aryan <47309918+winexe0@users.noreply.github.com>
2020-08-02 22:29:53 -0700

    Add files via upload

--- install-windows.bat
+++ install-windows.bat
@@ -1,16 +1,13 @@
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://osdn.net/frs/redir.php?m=xtom_us&f=mingw%2F68260%2Fmingw-get-setup.exe', 'minigw-get-setup.exe')" 
-mingw-get-setup
-echo Follow the MinGW Installation to compile dmg2iso.
-echo Don't change the Installation directory. It will cause problems. When It's done click on continue.
-echo Check the box on "mingw32-gcc-g++" and click on installation on the upper left corner.
-echo Click on apply changes and click on apply. When it is done, click on close.
+echo off
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://osdn.mirror.constant.com//mingw/68260/mingw-get-setup.exe', 'mingw-get-setup.exe')" 
 cd C:\MinGW\bin
-bitsadmin /transfer Code /priority high https://raw.githubusercontent.com/winexe0/dmg2iso/master/dmg2iso.cc dmg2iso.cc
-bitsadmin /transfer Code /priority high https://raw.githubusercontent.com/winexe0/dmg2iso/master/dmg2iso.h dmg2iso.h
-bitsadmin /transfer Code /priority high https://raw.githubusercontent.com/winexe0/dmg2iso/master/base64.h base64.h
-bitsadmin /transfer Code /priority high https://raw.githubusercontent.com/winexe0/dmg2iso/master/base64.cc base64.cc
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/winexe0/dmg2iso/master/dmg2iso.cc', 'dmg2iso.cc')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/winexe0/dmg2iso/master/dmg2iso.h', 'dmg2iso.h')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/winexe0/dmg2iso/master/base64.h', 'base64.h')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/winexe0/dmg2iso/master/base64.cc', 'base64.cc')"
 gcc -o dmg2iso.exe dmg2iso.o base64.o -L. -lz -s
 gcc -03 -c dmg2iso.cc
 gcc -03 -c base64.cc
 move dmg2iso.exe C:\Windows\System32
 echo You can close this window now
+pause
